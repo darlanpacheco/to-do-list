6730e7d1fcbb59f473c065f1dad762d7eab65589
Darlan Pacheco <143919135+darlanpacheco@users.noreply.github.com>
2024-06-28 22:37:10 +0000

    Switch theme button

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import Todo from "./components/Todo";
+import ThemeButton from "./components/ThemeButton";
 
-// THEME SWITCH BUTTON
 // TODOS SAVE
 
 const App: React.FunctionComponent = (): React.ReactNode => {
   const [todos, setTodos] = useState<string[]>([]);
 
   return (
-    <main className="bg-secondary dark:bg-secondary-dark lg:shadow-lg rounded-lg flex flex-col w-[500px] min-h-[550px] mt-[15vh] mb-[15vh]">
-      <Header todos={todos} setTodos={setTodos} className="row-span-1"></Header>
-      <section className="row-span-6 flex flex-col items-center py-10 pb-10">
-        <ul>
-          {todos.map((value, index) => (
-            <Todo key={index}>{value}</Todo>
-          ))}
-        </ul>
-      </section>
-    </main>
+    <>
+      <ThemeButton></ThemeButton>
+      <main className="bg-secondary dark:bg-secondary-dark lg:shadow-lg rounded-lg flex flex-col w-[500px] min-h-[550px] mt-[15vh] mb-[15vh]">
+        <Header
+          todos={todos}
+          setTodos={setTodos}
+          className="row-span-1"
+        ></Header>
+        <section className="row-span-6 flex flex-col items-center py-10 pb-10">
+          <ul>
+            {todos.map((value, index) => (
+              <Todo key={index}>{value}</Todo>
+            ))}
+          </ul>
+        </section>
+      </main>
+    </>
   );
 };
 
---
+++ src/components/ThemeButton.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import IonIcon from "@reacticons/ionicons";
+
+// THEME SWITCH BUTTON
+// TODOS SAVE
+
+const ThemeButton: React.FunctionComponent = (): React.ReactNode => {
+  const [theme, setTheme] = useState<boolean>(true);
+
+  const themeToggle = () => {
+    document.body.parentElement?.classList.toggle("dark", !theme);
+    setTheme(!theme);
+    console.log(theme);
+  };
+
+  return (
+    <button
+      onClick={themeToggle}
+      className="absolute dark:bg-quaternary dark:hover:bg-[#4E4E4E] bg-white w-10 h-10 right-[15px] top-[15px] flex items-center justify-center rounded-full"
+    >
+      {theme ? (
+        <IonIcon name="moon"></IonIcon>
+      ) : (
+        <IonIcon name="sunny"></IonIcon>
+      )}
+    </button>
+  );
+};
+
+export default ThemeButton;
